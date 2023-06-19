cc6eda7f1db5307a999dee75370cc850072d8b2e
anshgit101 <anshkhandelwal2001@gmail.com>
2023-06-19 22:55:12 +0530

    r_v4

--- assets/js/research.js
+++ assets/js/research.js
@@ -38,6 +38,24 @@ const research = [
       "This paper introduces an internet-of-things (IoT) based economic retrofitting setup for digitising the analog water meters to make them smart. The setup contains a Raspberry-Pi microcontroller and a Pi-camera mounted on top of the analog water meter to take its images. The captured images are then preprocessed to estimate readings using a machine learning (ML) model. The employed ML algorithm is trained on a rich dataset that includes digits from the images of water meters captured by the hardware setup for ten days. The readings are posted on a cloud server in real-time using Raspberry-Pi. High temporal resolution plots of flow rate and volume are generated to derive inferences. The collected data can be used for deriving water consumption patterns and fault detection for efficient water management.",
     absbox: "absPopup2",
   },
+
+  {
+    title: "System and Method for Digitizing in an Analog Water Meter using Machine Learning",
+    authors:
+      "Ayush Kumar Lall, Ansh Khandelwal, Rishi Bose, Nilesh Bawankar, Nitin Nilesh, Ayush Dwivedi and Sachin Chaudhari",
+    conferences:
+      "Indian Patent Office",
+    researchYr: 2022,
+    citebox: "popup2",
+    image: "assets/images/research-page/pp3.png",
+    citation: {
+      vancouver:
+      "A. K. Lall et al., Making Analog Water Meter Smart using ML and IoT-based Low-Cost Retrofitting, 2021 8th International Conference on Future Internet of Things and Cloud (FiCloud), Rome, Italy, 2021, pp. 157-162, doi: 10.1109/FiCloud49777.2021.00030.",
+    },
+    abstract:
+      "A retrofit system 100 and method for digitizing in an analog water meter using a machine learning model are provided. The retrofit system 100 includes a controller 102, a power supply module 104, a server 106, an image capturing device 108, a machine learning model 110, a light module 112, and a relay module 114. The image capturing device 108 captures one or more images of dial of the analog water meter. The light module 112 provides illumination to the image capturing device 108. The relay module 114 controls the light module 112. The power module 104 enables power to the retrofit system 100 without any interruption. The controller 102 detects reading of the analog water meter from the captured one or more images. The server 106 aggregates the detected readings and processes the aggregated data to digitize in the readings using machine learning model 110.",
+    absbox: "absPopup2",
+  },
 ];
 AOS.init();
 const fillData = () => {
